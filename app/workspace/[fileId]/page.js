34270ac4678b7782d1e5d81a0a44f040176c256b
Ashis-Mishra07 <mishralucky074@gmail.com>
2024-12-02 23:27:17 +0530

    pdf viewer added

--- app/workspace/[fileId]/page.js
+++ app/workspace/[fileId]/page.js
@@ -1,14 +1,36 @@
 "use client"
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 import WorkspaceHeader from '../_components/WorkspaceHeader';
+import PdfViewer from '../_components/PdfViewer';
+import { useQueries, useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 function Workspace() {
     const { fileId } = useParams();
+    const fileInfo = useQuery(api.fileStorage.GetFileRecord,{
+        fileId:fileId
+    });
 
+    useEffect(()=>{
+        console.log(fileInfo);
+        
+    },[fileInfo])
+    
   return (
     <div>
         <WorkspaceHeader />
+
+        <div className='grid grid-cols-2 gap-5 '>
+            <div>
+                {/* Text Editor */}
+
+            </div>
+            <div>
+                {/* Pdf Viewer */}
+                <PdfViewer fileUrl={fileInfo?.fileUrl}/>
+            </div>
+        </div>
     </div>
   )
 }
--- convex/fileStorage.js
+++ convex/fileStorage.js
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+import { handler } from "tailwindcss-animate";
 
 export const generateUploadUrl = mutation(async (ctx) => {
   return await ctx.storage.generateUploadUrl();
@@ -32,5 +33,18 @@ export const getFileUrl=mutation({
     handler:async(ctx,args)=>{
         const url = await ctx.storage.getUrl(args.storageId);
         return url;
+    }
+})
+
+export const GetFileRecord = query({
+    args:{
+        fileId:v.string()
+    },
+    handler:async(ctx,args)=>{
+        const result = await ctx.db.query('pdfFiles').filter((q)=>q.eq(q.field('fileId'),args.fileId))
+        .collect();
+        console.log(result);
+        
+        return result[0];
     }
 })
